af8e8a496c6f2a86775ad0fe1f3e3cbb90544176
liyixiang <394244674@qq.com>
2015-04-12 15:52:22 +0800

    init

---
+++ GraduationProject_3.0 Maven Webapp/mms-cache/src/main/java/com/letv/mms/App.java
@@ -0,0 +1,13 @@
+package com.letv.mms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
